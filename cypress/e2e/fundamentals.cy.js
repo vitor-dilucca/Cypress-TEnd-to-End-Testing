506f82a09d0feee677b870d3b8360159c7001f04
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-15 18:26:28 -0300

    primeiro test achando o h1

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/fundamentals')
+    //essas barras '/testing/i ' é um regex e serve para q n seja case sensitive
+    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
   })
 })
