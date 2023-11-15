801b246bd7f016cad6bb74bd7df168fa9adbd712
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-15 18:30:09 -0300

    setando o localhost3000 como url base

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'y6zja5',
   e2e: {
+    baseUrl:'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/fundamentals')
+describe('Fundamentals test', () => {
+  it('Contains correct header text', () => {
+    cy.visit('/fundamentals')
     //essas barras '/testing/i ' é um regex e serve para q n seja case sensitive
     cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
   })
