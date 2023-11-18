ff54f8238c01be730648c5382167e0ff40114755
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-18 15:42:27 -0300

    podemos criar comandos para selecionar elementos de maneira mais sucinta Ex: cy.getDataTest('fundamentals-header'), tem q editar na pagina commands.js

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -2,22 +2,24 @@ describe("Fundamentals test", () => {
   beforeEach(() => {
     cy.visit("/fundamentals");
   });
+
   it("Contains correct header text", () => {
+    cy.getDataTest('fundamentals-header')
     //essas barras '/testing/i ' é um regex e serve para q n seja case sensitive
-    cy.get('[data-test="fundamentals-header"]').contains(
-      /Testing Fundamentals/i
-    );
+    // cy.get('[data-test="fundamentals-header"]').should('contain.text',/Testing Fundamentals/i);
+    cy.getDataTest('fundamentals-header').should('contain.text',/Testing Fundamentals/i);
   });
+
   it.only("Accordion works correctly", () => {
     cy.contains(/Your tests will exist in a describe block/i).should(
       "not.be.visible"
     );
     cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
-    cy.contains(/Your tests will exist in a describe block/i).should(
+    cy.contains('Your tests will exist in a describe block').should(
       "be.visible"
     );
     cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
-    cy.contains(/Your tests will exist in a describe block/i).should(
+    cy.contains('Your tests will exist in a describe block').should(
       "not.be.visible"
     );
   });
