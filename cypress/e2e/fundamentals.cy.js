1819ad8b83e25760c6c9c42435d08ff24574543f
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-18 15:11:47 -0300

    clicando no acordeon abrindo e fechando

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -4,4 +4,12 @@ describe('Fundamentals test', () => {
     //essas barras '/testing/i ' é um regex e serve para q n seja case sensitive
     cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
   })
+  it('Accordion works correctly', () => {
+    cy.visit('/fundamentals')
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+  })
 })
