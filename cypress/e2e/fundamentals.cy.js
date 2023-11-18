706e730db237f30b2dba0b84c7fc23f497f679a4
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-18 15:28:47 -0300

    voce usa beforeEach para rodar um codigo antes de cada test, assim evita repetir o mesmo codigo no começo de todos os testes

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,15 +1,24 @@
-describe('Fundamentals test', () => {
-  it('Contains correct header text', () => {
-    cy.visit('/fundamentals')
+describe("Fundamentals test", () => {
+  beforeEach(() => {
+    cy.visit("/fundamentals");
+  });
+  it("Contains correct header text", () => {
     //essas barras '/testing/i ' é um regex e serve para q n seja case sensitive
-    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
-  })
-  it.only('Accordion works correctly', () => {
-    cy.visit('/fundamentals')
-    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
-    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
-    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
-    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
-    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
-  })
-})
+    cy.get('[data-test="fundamentals-header"]').contains(
+      /Testing Fundamentals/i
+    );
+  });
+  it.only("Accordion works correctly", () => {
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+  });
+});
