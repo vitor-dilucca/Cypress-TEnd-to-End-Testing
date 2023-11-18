f8bb35e814893a3f8a3b5f3a643223da25e91b5d
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-18 16:25:10 -0300

    teste de inserir email valido no input, e ensinando a usar alias, para referenciar elementos d maneira mais sucinta

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -2,7 +2,17 @@ describe('form test', ()=>{
   beforeEach(()=>{
     cy.visit('/forms')
   })
+
   it('Test subscribe form',()=>{
-    cy.contains('Testing Forms')
+      cy.contains('Testing Forms')
+      //usando .as vc cria uma alias, que serve para referenciar uma linha de codigo
+      cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+      cy.get('@subscribe-input').type('zyzzelxulo9@gmail.com')
+      cy.contains('Successfully subbed').should('not.exist')
+      cy.getDataTest('subscribe-button').click()
+      cy.contains('Successfully subbed').should('exist')
+      cy.wait(3000)
+
   })
+
 })
