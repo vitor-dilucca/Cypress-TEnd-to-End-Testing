aaa7ff6b167e408e04e7555cf7e8343d64daf409
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-19 09:32:16 -0300

    fazendo alguns testes mais no acordion, abrindo o acordion e vendo se o conteudo dele esta visivel

--- cypress/component/Accordion.cy.jsx
+++ cypress/component/Accordion.cy.jsx
@@ -31,8 +31,24 @@ const items = [
 describe("Accordion.cy.jsx", () => {
   it("Items accordion", () => {
     cy.mount(<ItemsAccordion items={items} />);
-    cy.getDataTest('accordion-wrapper').within(()=>{
-      cy.get('[data-test^="accordion-item"]').should('have.length',3)
-    })
+    cy.getDataTest("accordion-wrapper").within(() => {
+      cy.get('[data-test^="accordion-item"]').should("have.length", 3);
+    });
+
+    cy.contains("Your tests will exist in a describe block").should(
+      "not.be.visible"
+    );
+    cy.getDataTest("accordion-item-1").within(() =>
+      cy.get("[role=button]").click()
+    );
+    cy.contains("Your tests will exist in a describe block").should(
+      "be.visible"
+    );
+    cy.getDataTest("accordion-item-1").within(() =>
+      cy.get("[role=button]").click()
+    );
+    cy.contains("Your tests will exist in a describe block").should(
+      "not.be.visible"
+    );
   });
 });
