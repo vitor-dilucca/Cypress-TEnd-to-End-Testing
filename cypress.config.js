e94267fcf3477d48cc0d80fe9b94f7ccca1527f2
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-19 09:26:09 -0300

    fazendo Teste de component individual, checando se existem 3 elementos no acordion

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'y6zja5',
+  projectId: "y6zja5",
+
   e2e: {
-    baseUrl:'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
   },
 });
